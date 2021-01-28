1424ea55b5a37edef70d5b191134a45bcb37e562
SHIV5T3R <44001656+SHIV5T3R@users.noreply.github.com>
2021-01-28 10:31:02 +0000

    Update rpresence.py

--- ShivBox/rpresence.py
+++ ShivBox/rpresence.py
@@ -45,23 +45,3 @@ class discordRPC:
         print('OP Code: {}; Length: {}\nResponse:\n{}\n'.format(
             code, length, json.loads(data[8:].decode('utf-8'))))
 
-    def update(self, activity):
-        current_time = time.time()
-        payload = {
-            "cmd": "SET_ACTIVITY",
-            "args": {
-                "activity": activity,
-                "pid": os.getpid()
-            },
-            "nonce": '{:.20f}'.format(current_time)
-        }
-        print("sending data")
-        sent = self.send_data(1, payload)
-        self.loop.run_until_complete(self.read_output())
-
-    def close(self):
-        self.sock_writer.close()
-        self.loop.close()
-
-    def start(self):
-        self.loop.run_until_complete(self.handshake())
