93f43f394e26e0e6f730ba761dd2f018f624ff5c
SHIV5T3R <44001656+SHIV5T3R@users.noreply.github.com>
2021-01-28 11:06:01 +0000

    Create client.py

---
+++ ShivBox/client.py
@@ -0,0 +1,22 @@
+cla
+
+def send_rp(self, activity):
+        current_time = time.time()
+        payload = {
+            "cmd": "SET_ACTIVITY",
+            "args": {
+                "activity": activity,
+                "pid": os.getpid()
+            },
+            "nonce": '{:.20f}'.format(current_time)
+        }
+        print("sending data")
+        sent = self.send_data(1, payload)
+        self.loop.run_until_complete(self.read_output())
+
+    def close(self):
+        self.sock_writer.close()
+        self.loop.close()
+
+    def start(self):
+        self.loop.run_until_complete(self.make_pc())
